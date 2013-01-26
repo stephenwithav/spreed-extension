991897730b34eeff2be92b3c84e32891de98972c
Troy Shu <tmshu1@gmail.com>
2013-01-26 13:47:06 -0500

    implement static window sizing

--- menu.js
+++ menu.js
@@ -1,8 +1,16 @@
 function callbackTestFunction(obj) { //works
   localStorage.setItem("selectedText", obj.selectionText);
   //alert(localStorage.getItem("selectedText")); 
-  
-  popupwindow("app.html", "", 700, 400);
+  //window width height
+    var width = 700;
+    var height = 400;
+    if (localStorage.getItem("width")> 0 && localStorage.getItem("height")>0) {
+      width = localStorage.getItem("width");
+      height=localStorage.getItem("height");
+    }
+    width = parseInt(width);
+    height = parseInt(height);
+  popupwindow("app.html", "", width, height);
 }
 
 function popupwindow(url, title, w, h) {
